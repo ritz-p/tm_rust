6b3b093305e7b7933ee529a6229b7dc5d54f13a4
DESKTOP-GC4IVE1\Ritz <okooko24816@gmail.com>
2023-10-01 17:20:59 +0900

    fix: 座標

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ fn main() {
     let mut stdout = stdout().into_raw_mode().unwrap();
     write!(stdout, "{}{}", clear::All, cursor::Hide).unwrap();
     let _ = stdout.flush();
-
-    let hundread_millis = time::Duration::from_millis(100);
+    let one_millis = time::Duration::from_millis(10);
+    let hundread_millis = time::Duration::from_millis(90);
     let mut res:Vec<String> = vec!["".to_string();10];
     let ti = character::ti();
     let nn = character::nn();
@@ -19,12 +19,20 @@ fn main() {
     for i in 0..10{
         res[i] = format!("{} {} {} {} {} {} {} {}",ti[i],nn[i],ti[i],nn[i],mo[i],ti[i],mo[i],ti[i]);
     }
-    let len = res[0].len();
-    for i in 0..len{
+    let max = res[0].len();
+    let pos = terminal_size().unwrap();
+    let x = pos.0;
+    let y = pos.1;
+    for i in 0..x{
         for j in 1..=10{
             let _ = stdout.flush();
-            write!(stdout,"{}",cursor::Goto(0,j)).unwrap();
-            write!(stdout,"{}",&res[(j as usize)-1][0..=(i as usize)]).unwrap();
+            thread::sleep(one_millis);
+            write!(stdout,"{}",cursor::Goto(x - i,y/2 - 5 + j)).unwrap();
+            if max < i as usize{
+                write!(stdout,"{}",&res[(j as usize)-1][0..max]).unwrap();
+            }else{
+                write!(stdout,"{}",&res[(j as usize)-1][0..(i as usize)]).unwrap();
+            }
         }
         thread::sleep(hundread_millis);
     }
